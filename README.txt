cf2e6258933488b05512f80141529450e7544a95
R4WM4T <81012587+R4WM4T@users.noreply.github.com>
2021-03-20 11:09:15 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+Ce programme python permet la création de listes de caractères ("wordlist") qui sont rédigées et stockées sous la forme
+d'un ficher texte (.txt). Ici, il permet l'obtention d'une wordlist de chaînes de 5 caractères.
+Ces listes sont utilisées notamment pour cracker des mots de passe en essayant les différentes combinaisons de caractères 
+possibles pour une longueur donnée.
+
+Cependant, ce programme reste gourmand en ressources de disque c'est la raison pour laquelle il est préférable de ne pas
+l'exécuter sur son ordinateur personel avec une longueur donnée trop importante.
---
+++ wordlist.py
@@ -0,0 +1,15 @@
+﻿character_possibles = ['a',"b",'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9]
+fichier = open("wordlist.txt", "w")
+
+for i in range(0,len(character_possibles)):
+    for j in range(0,len(character_possibles)):
+        for k in range(0,len(character_possibles)):
+            for l in range(0,len(character_possibles)):
+                for m in range(0,len(character_possibles)):
+                    fichier.write(str(character_possibles[i]))
+                    fichier.write(str(character_possibles[j]))
+                    fichier.write(str(character_possibles[k]))
+                    fichier.write(str(character_possibles[l]))
+                    fichier.write(str(character_possibles[m]))
+                    fichier.write("\n")
+fichier.close()
